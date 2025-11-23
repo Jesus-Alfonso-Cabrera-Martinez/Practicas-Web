f32bcd41101853d1e0b1c3b198ef346b7c888fdf
Jesus-Alfonso-Cabrera-Martinez <a2233336118@alumnos.uat.edu.mx>
2025-11-23 16:56:26 -0600

    Add files via upload

---
+++ P1 WEB/app/Http/Controllers/PaginaController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaginaController extends Controller
+{
+    public function bienvenida()
+    {
+        return view('bienvenida');
+    }
+
+    public function saludo($nombre)
+    {
+        return view('saludo', ['nombre' => $nombre]);
+    }
+}
